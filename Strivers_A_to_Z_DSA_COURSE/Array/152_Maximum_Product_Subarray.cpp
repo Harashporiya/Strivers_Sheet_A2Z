243e69d3ffc106acff2835e9dad23f4a24c39ca0
Harash <Harashporiya>
2024-09-27 09:01:52 +0530

    update question maximum product with subarray

--- Strivers_A_to_Z_DSA_COURSE/Array/152_Maximum_Product_Subarray.cpp
+++ Strivers_A_to_Z_DSA_COURSE/Array/152_Maximum_Product_Subarray.cpp
@@ -1,32 +1,41 @@
 #include <iostream>
+#include<bits/stdc++.h>
 #include <vector>
 using namespace std;
 int maxProduct(vector<int> &a, int n)
 {
-    int maxi = 0;
+    int maxi = INT_MIN;
     int product = 1;
-    for (int i = 0; i < n; i++)
-    {
-        product *= a[i];
+    // for (int i = 0; i < n; i++)
+    // {
+    //     product *= a[i];
 
-        maxi = max(maxi, product);
+    //     maxi = max(maxi, product);
 
-        if (product == 0)
-        {
-            product = 1;
-        }
-    }
-    product = 1;
-    for (int i = n - 1; i >= 0; i--)
-    {
-        product *= a[i];
+    //     if (product == 0)
+    //     {
+    //         product = 1;
+    //     }
+    // }
+    // product = 1;
+    // for (int i = n - 1; i >= 0; i--)
+    // {
+    //     product *= a[i];
 
-        maxi = max(maxi, product);
+    //     maxi = max(maxi, product);
 
-        if (product == 0)
-        {
-            product = 1;
-        }
+    //     if (product == 0)
+    //     {
+    //         product = 1;
+    //     }
+    // }
+    int suff = 1;
+    for(int i=0;i<n;i++){
+        if(product==0) product=1;
+        if(suff==0) suff=1;
+        product*=a[i];
+        suff*=a[n-i-1];
+        maxi=max(maxi, max(product,suff));
     }
     return maxi;
 }
