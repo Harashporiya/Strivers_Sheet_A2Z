c85a0fb23611380bf655278a0e353e2e56ff49ce
Harash <harash.poriya@gmail.com>
2024-10-02 16:47:17 +0530

    ceil elements find in sorted array

---
+++ Strivers_A_to_Z_DSA_COURSE/BinarySearch/ceilFind.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int binarySearch(vector<int> &a, int n, int k){
+    int low=0;
+    int high=n-1;
+    int ans = -1;
+    while(low<=high){
+        int mid = low+(high-low)/2;
+        if(a[mid]<=k){        
+           low=mid+1;
+        }else{
+            ans = a[mid];
+            high=mid-1;
+        }
+    }
+    return ans;
+}
+int main(){
+    int n,k;
+    cin>>n>>k;
+    vector<int> a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+
+    int ans = binarySearch(a,n,k);
+    cout<<ans;
+}
