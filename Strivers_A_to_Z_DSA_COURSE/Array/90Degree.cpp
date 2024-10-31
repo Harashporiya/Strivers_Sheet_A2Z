a0a03484711af47fefa5fcac49f902dee6cdb562
Harash <harash.poriya@gmail.com>
2024-10-31 10:00:30 +0530

    commit

--- Strivers_A_to_Z_DSA_COURSE/Array/90Degree.cpp
+++ Strivers_A_to_Z_DSA_COURSE/Array/90Degree.cpp
@@ -1,16 +1,16 @@
 #include<iostream>
 using namespace std;
-void reverse1(int a[][], int n){
-    int low=0;
-    int high=n-1;
-    while(low<=high){
-        int t=a[low];
-        a[low]=a[high];
-        a[high]=t;
-        low++;
-        high--;
-    }
-}
+// void reverse1(int a[100][100], int n){
+//     int low=0;
+//     int high=n-1;
+//     while(low<=high){
+//         int t=a[low];
+//         a[low]=a[high];
+//         a[high]=t;
+//         low++;
+//         high--;
+//     }
+// }
 int main() {
     int n, m;
     cin >> n >> m;
@@ -28,16 +28,16 @@ int main() {
     //       ans[j][n-1-i] = a[i][j];
     //     }
     // }
-    for(int i=0;i<n-1;i++){
+    for(int i=0;i<n;i++){
         for(int j=i+1;j<m;j++){
             int t = a[i][j];
             a[i][j]=a[j][i];
-            a[j][i]=a[i][j];
+            a[j][i]=t;
         }
     }
-    for(int i=0;i<n;i++){
-        reverse1(a, n);
-    }
+    // for(int i=0;i<n;i++){
+    //     reverse1(a, n);
+    // }
     
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
