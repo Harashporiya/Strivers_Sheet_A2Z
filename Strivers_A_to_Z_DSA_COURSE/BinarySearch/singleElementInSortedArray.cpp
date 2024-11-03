091d30529bba028931c150e9c8cf2d30ca2deee0
Harash <harash.poriya@gmail.com>
2024-11-03 09:12:36 +0530

    540. Single Element in a Sorted Arraytransaction route

--- Strivers_A_to_Z_DSA_COURSE/BinarySearch/singleElementInSortedArray.cpp
+++ Strivers_A_to_Z_DSA_COURSE/BinarySearch/singleElementInSortedArray.cpp
@@ -1,25 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
-int singleElementSortedArray(vector<int> &a, int n){
-   if(n==1) return a[0];
-   if(a[0]!=a[1]) return a[0];
-   if(a[n-1]!=a[n-2]) return a[n-1];
-     int low=1;
-   int high=n-2;
-   while(low<=high){
-         int mid = low+(high-low)/2;
-         if(a[mid]!=a[mid+1] && a[mid]!=a[mid-1]){
-            return a[mid];
-         }
+// int singleElementSortedArray(vector<int> &a, int n){
+//    if(n==1) return a[0];
+//    if(a[0]!=a[1]) return a[0];
+//    if(a[n-1]!=a[n-2]) return a[n-1];
+//      int low=1;
+//    int high=n-2;
+//    while(low<=high){
+//          int mid = low+(high-low)/2;
+//          if(a[mid]!=a[mid+1] && a[mid]!=a[mid-1]){
+//             return a[mid];
+//          }
 
-         if((mid%2==1 && a[mid-1]==a[mid]) || (mid%2==0 && a[mid]==a[mid+1]) ){
-            low=mid+1;
-         }else{
-            high=mid-1;
-         }
-   }
-   return -1;
-}
+//          if((mid%2==1 && a[mid-1]==a[mid]) || (mid%2==0 && a[mid]==a[mid+1]) ){
+//             low=mid+1;
+//          }else{
+//             high=mid-1;
+//          }
+//    }
+//    return -1;
+// }
 int main(){
     int n;
     cin>>n;
@@ -28,6 +28,16 @@ int main(){
         cin>>a[i];
     }
 
-    int ans = singleElementSortedArray(a,n);
-    cout<<ans;
+   //  int ans = singleElementSortedArray(a,n);
+   //  cout<<ans;
+   map<int, int> mpp;
+   for(int i=0;i<n;i++){
+      mpp[a[i]]++;
+   }
+
+   for(auto it:mpp){
+      if(it.second == 1){
+         cout<<it.first;
+      }
+   }
 }
