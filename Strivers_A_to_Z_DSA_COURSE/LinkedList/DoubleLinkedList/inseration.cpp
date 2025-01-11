76473c0a7ca8677d102f4d33254b984fb7809247
harash <harash.poriya@gmail.com>
2025-01-11 10:52:58 +0530

    insert before head

---
+++ Strivers_A_to_Z_DSA_COURSE/LinkedList/DoubleLinkedList/inseration.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node *next;
+    Node* back;
+    Node(int val){
+        data = val;
+        next = NULL;
+        back = NULL;
+    }
+    Node(int val, Node* next1, Node* back1){
+        data = val;
+        next = next1;
+        back = back1;
+    }
+};
+void printLinked(Node *head){
+    while(head!=NULL){
+        cout<<head->data<<" ";
+        head=head->next;
+    }
+    cout<<endl;
+}
+Node* insertBeforeHead(Node* head, int val){
+    Node* newHead = new Node(val);
+    newHead->next=head;
+    newHead->back=NULL;
+    head->back=newHead;
+
+    return newHead;
+}
+int main(){
+    int n;
+    cin>>n;
+    Node *head = NULL;
+    Node *temp=NULL;
+    for(int i=0;i<n;i++){
+        int value;
+        cin>>value;
+        Node *newNode = new Node(value);
+        if(head==NULL){
+            head = newNode;
+        }else{
+            temp->next=newNode;
+            newNode->back = temp;
+        }
+        temp=newNode;
+    }
+   head=insertBeforeHead(head,0);
+    printLinked(head);
+}
