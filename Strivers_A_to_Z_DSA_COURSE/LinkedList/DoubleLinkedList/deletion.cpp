e73d477b0f83dea0241ba8d8087d04ed94cf6b97
harash <harash.poriya@gmail.com>
2025-01-11 09:31:02 +0530

    Delete first Node

---
+++ Strivers_A_to_Z_DSA_COURSE/LinkedList/DoubleLinkedList/deletion.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node *next;
+    Node* back;
+    Node(int val){
+        data = val;
+        next = NULL;
+        back = NULL;
+    }
+    Node(int val, Node* next1, Node* back1){
+        data = val;
+        next = next1;
+        back = back1;
+    }
+};
+void printLinked(Node *head){
+    while(head!=NULL){
+        cout<<head->data<<" ";
+        head=head->next;
+    }
+    cout<<endl;
+}
+Node* deleteNode(Node* head){
+    if(head==NULL || head->next==NULL){
+        return NULL;
+    }
+    Node* prev = head;
+    head=head->next;
+
+    head->back=NULL;
+    prev->next=NULL;
+
+    delete prev;
+    return head;
+}
+int main(){
+    int n;
+    cin>>n;
+    Node *head = NULL;
+    Node *temp=NULL;
+    for(int i=0;i<n;i++){
+        int value;
+        cin>>value;
+        Node *newNode = new Node(value);
+        if(head==NULL){
+            head = newNode;
+        }else{
+            temp->next=newNode;
+            newNode->back = temp;
+        }
+        temp=newNode;
+    }
+   head=deleteNode(head);
+    printLinked(head);
+}
