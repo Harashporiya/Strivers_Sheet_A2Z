376cec597f2dd35bcc4cee1411749158b3518317
harash <harash.poriya@gmail.com>
2025-01-13 16:25:55 +0530

    Sort 0's and 1's and 2's this code time complexity o(n) and before code time complexity is o(2n)

--- Strivers_A_to_Z_DSA_COURSE/LinkedList/SingleLinkedList/Sort1and2and3.cpp
+++ Strivers_A_to_Z_DSA_COURSE/LinkedList/SingleLinkedList/Sort1and2and3.cpp
@@ -17,34 +17,66 @@ void printLinked(Node *head){
     cout<<endl;
 }
 Node* sortOneAndTwoandZero(Node* head){
-  int zeroCnt=0,oneCnt=0,twoCnt=0;
-  Node* temp=head;
-  while(temp!=NULL){
-    if(temp->data==0){
-        zeroCnt++;
-    }else if(temp->data==1){
-        oneCnt++;
-    }else if(temp->data==2){
-        twoCnt++;
-    }
-    temp=temp->next;
-  }
-  temp=head;
-  while(temp!=NULL){
-    if(zeroCnt){
-        temp->data=0;
-        zeroCnt--;
-    }else if(oneCnt){
-        temp->data=1;
-        oneCnt--;
-    }else if(twoCnt){
-        temp->data=2;
-        twoCnt--;
-    }
-    temp=temp->next;
+ // int zeroCnt=0,oneCnt=0,twoCnt=0;
+//   Node* temp=head;
+//   while(temp!=NULL){
+//     if(temp->data==0){
+//         zeroCnt++;
+//     }else if(temp->data==1){
+//         oneCnt++;
+//     }else if(temp->data==2){
+//         twoCnt++;
+//     }
+//     temp=temp->next;
+//   }
+//   temp=head;
+//   while(temp!=NULL){
+//     if(zeroCnt){
+//         temp->data=0;
+//         zeroCnt--;
+//     }else if(oneCnt){
+//         temp->data=1;
+//         oneCnt--;
+//     }else if(twoCnt){
+//         temp->data=2;
+//         twoCnt--;
+//     }
+//     temp=temp->next;
 
-  }
-  return head;
+//   }
+//   return head;
+   Node* zeroHead = new Node(-1);
+   Node* oneHead = new Node(-1);
+   Node* twoHead = new Node(-1);
+   Node* zero = zeroHead;
+   Node* one = oneHead;
+   Node* two = twoHead;
+   Node* temp = head;
+   while(temp!=NULL){
+    if(temp->data==0){
+        zero->next=temp;
+        zero=zero->next;
+    }else if(temp->data==1){
+        one->next=temp;
+        one=one->next;
+    }else{
+        two->next=temp;
+        two=two->next;
+    }
+    temp=temp->next;
+   }
+   zero->next = (oneHead->next) ? (oneHead->next) : (twoHead->next);
+   one->next = twoHead->next;
+   twoHead->next = NULL;
+
+   Node* newHead = zeroHead->next;
+
+   delete zeroHead;
+   delete oneHead;
+   delete twoHead;
+
+   return newHead;
+
 }
 int main(){
     int n;
