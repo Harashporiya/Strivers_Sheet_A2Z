9a635ea16c46b4a01fe8da54a4759b2a0e5a4818
harash <harash.poriya@gmail.com>
2025-01-16 16:02:54 +0530

    Merge k Sorted Lists use priority queue

--- Strivers_A_to_Z_DSA_COURSE/LinkedList/SingleLinkedList/MergekSortedLists.cpp
+++ Strivers_A_to_Z_DSA_COURSE/LinkedList/SingleLinkedList/MergekSortedLists.cpp
@@ -22,80 +22,99 @@ void printLinked(Node *head)
     }
     cout << endl;
 }
-Node* mergeLinkedList(Node* left, Node* right){
- Node* dummyNode = new Node(-1);
- Node* temp=dummyNode;
-  while(left!=NULL && right!=NULL){
-    if(left->data < right->data){
-        temp->next=left;
-        temp=left;
-        left=left->next;
-    }else{
-        temp->next=right;
-        temp=right;
-        right=right->next;
+// Node* mergeLinkedList(Node* left, Node* right){
+//  Node* dummyNode = new Node(-1);
+//  Node* temp=dummyNode;
+//   while(left!=NULL && right!=NULL){
+//     if(left->data < right->data){
+//         temp->next=left;
+//         temp=left;
+//         left=left->next;
+//     }else{
+//         temp->next=right;
+//         temp=right;
+//         right=right->next;
+//     }
+//   }
+//    if(left){
+//         temp->next=left;
+//     }else{
+//         temp->next=right;
+//     }
+//     return dummyNode->next;
+// }
+// Node* findMiddleLinkedList(Node* head){
+//     Node* temp=head;
+//     Node* slow=head;
+//     Node* fast=head->next;
+//     while(fast!=NULL && fast->next!=NULL){
+//         slow=slow->next;
+//         fast=fast->next->next;
+//     }
+//     return slow;
+// }
+// Node* sortLinkedList(Node* head){
+//     if (!head || !head->next) {
+//     return head;
+// }
+//    Node* temp=head;
+//   Node* middle= findMiddleLinkedList(head);
+//   Node* right=middle->next;
+//   middle->next=NULL;
+//   Node* left=head;
+
+//   left=sortLinkedList(left);
+//   right=sortLinkedList(right);
+
+//   return mergeLinkedList(left, right);
+// }
+// Node *multiLinkedlistOnlyOneLinkedListPrint(vector<Node *> &lists)
+// {
+//     Node *mergedHead = NULL;
+//     Node *mergedTail = NULL;
+
+//     for (Node* list : lists)
+//     {
+//         if (mergedHead == NULL)
+//         {
+//             mergedHead = list;
+//             mergedTail = list;
+//             while (mergedTail->next != NULL)
+//             {
+//                 mergedTail = mergedTail->next;
+//             }
+//         }
+//         else
+//         {
+//             mergedTail->next = list;
+//             while (mergedTail->next != NULL)
+//             {
+//                 mergedTail = mergedTail->next;
+//             }
+//         }
+//     }
+//      return sortLinkedList(mergedHead);
+// }
+Node* mergedK(vector<Node*> lists){
+    priority_queue<pair<int, Node*>, vector<pair<int, Node*>>, greater<pair<int, Node*>>> minHeap;
+    for(int i=0;i<lists.size();i++){
+        if(lists[i]){
+            minHeap.push({lists[i]->data, lists[i]});
+        }
     }
-  }
-   if(left){
-        temp->next=left;
-    }else{
-        temp->next=right;
+    Node* dummyNode = new Node(-1);
+    Node* temp = dummyNode;
+    while(!minHeap.empty()){
+        auto it = minHeap.top();
+        minHeap.pop();
+        if(it.second->next){
+            minHeap.push({it.second->next->data,it.second->next});
+        }
+        temp->next=it.second;
+        temp=temp->next;
     }
     return dummyNode->next;
 }
-Node* findMiddleLinkedList(Node* head){
-    Node* temp=head;
-    Node* slow=head;
-    Node* fast=head->next;
-    while(fast!=NULL && fast->next!=NULL){
-        slow=slow->next;
-        fast=fast->next->next;
-    }
-    return slow;
-}
-Node* sortLinkedList(Node* head){
-    if (!head || !head->next) {
-    return head;
-}
-   Node* temp=head;
-  Node* middle= findMiddleLinkedList(head);
-  Node* right=middle->next;
-  middle->next=NULL;
-  Node* left=head;
-
-  left=sortLinkedList(left);
-  right=sortLinkedList(right);
-
-  return mergeLinkedList(left, right);
-}
-Node *multiLinkedlistOnlyOneLinkedListPrint(vector<Node *> &lists)
-{
-    Node *mergedHead = NULL;
-    Node *mergedTail = NULL;
-
-    for (Node* list : lists)
-    {
-        if (mergedHead == NULL)
-        {
-            mergedHead = list;
-            mergedTail = list;
-            while (mergedTail->next != NULL)
-            {
-                mergedTail = mergedTail->next;
-            }
-        }
-        else
-        {
-            mergedTail->next = list;
-            while (mergedTail->next != NULL)
-            {
-                mergedTail = mergedTail->next;
-            }
-        }
-    }
-     return sortLinkedList(mergedHead);
-}
-
 Node *createLinkedList(vector<int> &values)
 {
     Node *head = NULL;
@@ -133,7 +152,7 @@ int main()
         }
         lists.push_back(createLinkedList(values));
     }
-    Node* mergedList = multiLinkedlistOnlyOneLinkedListPrint(lists);
+    Node* mergedList = mergedK(lists);
     printLinked(mergedList); 
 
 
