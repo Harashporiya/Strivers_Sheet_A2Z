f7ae1fa7dd97ca3ef8deaed0e51a9a27106833b9
harash <harash.poriya@gmail.com>
2025-01-26 11:48:10 +0530

    Prefix to Postfix

---
+++ Strivers_A_to_Z_DSA_COURSE/Stack/PrefixToPostFix.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+string prefixToPostfix(string s){
+    stack<string> st;
+    for(int i=s.size()-1;i>=0;i--){
+        if((s[i]>='A'&&s[i]<='Z')||(s[i]>='a'&&s[i]<='z')||(s[i]>='0'&&s[i]<='9')){
+            st.push(string(1,s[i]));
+        }else{
+            string t1=st.top();
+            st.pop();
+            string t2=st.top();
+            st.pop();
+            string result = t1 + t2+s[i];
+            st.push(result);
+        }
+    }
+    return st.top();
+}
+int main(){
+    string str;
+    cin>>str;
+    cout<<prefixToPostfix(str);
+}
