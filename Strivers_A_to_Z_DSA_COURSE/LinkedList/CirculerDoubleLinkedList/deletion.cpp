141f279e893da2fc40220d5609ab5a405c59443d
harash <harash.poriya@gmail.com>
2025-03-07 18:13:43 +0530

    Double criculer linked list delete at head

---
+++ Strivers_A_to_Z_DSA_COURSE/LinkedList/CirculerDoubleLinkedList/deletion.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node* back;
+    Node(int v){
+        data = v;
+        next = NULL;
+        back = NULL;
+    }
+};
+void printLinkedList(Node* head){
+    Node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+        if(temp==head){
+            break;
+        }
+    }
+    cout<<endl;
+}
+Node* deleteAtHead(Node* head){
+    Node* temp=head->next;
+    Node* p = head->back;
+    temp->back = p;
+   
+    p->next = temp;
+    // head->back = head->next->next;
+    delete head;
+    return temp;
+}
+int main(){
+    int n;
+    cin>>n;
+    Node* head = NULL;
+    Node* temp = NULL;
+    for(int i=0;i<n;i++){
+        int v;
+        cin>>v;
+        Node* newNode = new Node(v);
+        if(head==NULL){
+            head = newNode;
+        }else{
+            temp->next = newNode;
+            newNode->back = temp;
+        }
+        temp=newNode;
+    }
+    if(temp != NULL) {
+        temp->next = head;
+         head->back = temp;
+    }
+
+  head=deleteAtHead(head);
+    printLinkedList(head);
+}
