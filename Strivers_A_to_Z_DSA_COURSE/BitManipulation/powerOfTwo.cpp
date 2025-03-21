33a5f5b28240367eed483b0a59ccac44c27936d6
harash <harash.poriya@gmail.com>
2025-03-21 17:21:35 +0530

    update code power of two

--- Strivers_A_to_Z_DSA_COURSE/BitManipulation/powerOfTwo.cpp
+++ Strivers_A_to_Z_DSA_COURSE/BitManipulation/powerOfTwo.cpp
@@ -1,7 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 int powerOfTwo(int n){
-    return (n > 0) && ((n & (n - 1)) == 0);
+    // return (n > 0) && ((n & (n - 1)) == 0);
+     if(n==0){
+            return 0;
+        }else if(n==1){
+            return 1;
+        }
+
+        return (n%2==0) && powerOfTwo(n/2);
 }
 int main(){
     int n;
