da7633f47c11f8bf3c7e5976426eef6f0da80606
harash <harash.poriya@gmail.com>
2025-03-21 17:35:28 +0530

    update code number of 1 bits try the code bitmanipulation

--- Strivers_A_to_Z_DSA_COURSE/BitManipulation/hammingWeight.cpp
+++ Strivers_A_to_Z_DSA_COURSE/BitManipulation/hammingWeight.cpp
@@ -1,21 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 int hammingWeight(int n) {
-        string ans;
-        while(n!=0){
-            if(n%2==1){
-                ans+="1";
-            }else{
-                ans+="0";
-            }
-            n/=2;
-        }
-        reverse(ans.begin(),ans.end());
+        // string ans;
+        // while(n!=0){
+        //     if(n%2==1){
+        //         ans+="1";
+        //     }else{
+        //         ans+="0";
+        //     }
+        //     n/=2;
+        // }
+        // reverse(ans.begin(),ans.end());
+        // int cnt=0;
+        // for(int i=0;i<ans.length();i++){
+        //     if(ans[i]=='1'){
+        //         cnt++;
+        //     }
+        // }
+        // return cnt;
         int cnt=0;
-        for(int i=0;i<ans.length();i++){
-            if(ans[i]=='1'){
-                cnt++;
-            }
+        while(n>0){
+            cnt += n & 1;
+            n>>=1;
         }
         return cnt;
 }
