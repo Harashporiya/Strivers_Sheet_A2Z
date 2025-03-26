679213c2e29f523fdd5203376bb5247908c324a8
harash <harash.poriya@gmail.com>
2025-03-26 18:14:09 +0530

    Inorder binary tree traversal

---
+++ Strivers_A_to_Z_DSA_COURSE/BinaryTree/inOrderTraversal.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data=val;
+        left=NULL;
+        right=NULL;
+    }
+};
+void inOrder(Node* root){
+    if(root==NULL){
+        return;
+    }
+    inOrder(root->left);
+    cout<<root->data<<" ";
+    inOrder(root->right);
+}
+int main(){
+   Node* root = new Node(10);
+   root->left = new Node(12);
+   root->right = new Node(5);
+   root->left->left = new Node(3);
+   root->left->right = new Node(4);
+   root->left->right->left = new Node(6);
+   root->left->right->right = new Node(7);
+   root->right->left = new Node(5);
+   root->right->left = new Node(11);
+   root->right->right = new Node(2);
+   root->right->right->right = new Node(8);
+
+   cout << "Inorder traversal: ";
+    inOrder(root); 
+}
