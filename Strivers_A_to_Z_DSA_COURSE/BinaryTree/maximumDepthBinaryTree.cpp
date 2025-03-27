648f9f69a816a4de717b03e403e07716d014f084
harash <harash.poriya@gmail.com>
2025-03-27 17:27:26 +0530

    level order count maximum depth

---
+++ Strivers_A_to_Z_DSA_COURSE/BinaryTree/maximumDepthBinaryTree.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data=val;
+        left=NULL;
+        right=NULL;
+    }
+};
+int maximumDepth(Node* root){
+    if(root==NULL){
+        return 0;
+    }
+    int cnt=0;
+    queue<Node*> q;
+    q.push(root);
+    while(!q.empty()){
+        int size=q.size();
+       
+        for(int i=0;i<size;i++){
+            Node* node = q.front();
+            q.pop();
+            if(node->left !=NULL){
+                q.push(node->left);
+            }
+            if(node->right!=NULL){
+                q.push(node->right);
+            }
+        
+        }
+        cnt++;
+    }
+    return cnt;
+   
+}
+int main(){
+   Node* root = new Node(10);
+   root->left = new Node(12);
+   root->right = new Node(5);
+   root->left->left = new Node(3);
+   root->left->right = new Node(4);
+   root->left->right->left = new Node(6);
+   root->left->right->right = new Node(7);
+   root->right->left = new Node(5);
+   root->right->left = new Node(11);
+   root->right->right = new Node(2);
+   root->right->right->right = new Node(8);
+
+  cout<<maximumDepth(root); 
+}
