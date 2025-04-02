29e7e9ffcefb95ec12022394066ffe8dc2dd0fe3
harash <harash.poriya@gmail.com>
2025-04-02 16:58:56 +0530

    Input change

--- Strivers_A_to_Z_DSA_COURSE/BinaryTree/maximumDepthBinaryTree.cpp
+++ Strivers_A_to_Z_DSA_COURSE/BinaryTree/maximumDepthBinaryTree.cpp
@@ -44,17 +44,11 @@ int maximumDepth(Node* root){
    
 }
 int main(){
-   Node* root = new Node(10);
-   root->left = new Node(12);
-   root->right = new Node(5);
-   root->left->left = new Node(3);
-   root->left->right = new Node(4);
-   root->left->right->left = new Node(6);
-   root->left->right->right = new Node(7);
-   root->right->left = new Node(5);
-   root->right->left = new Node(11);
-   root->right->right = new Node(2);
-   root->right->right->right = new Node(8);
+   Node* root = new Node(3);
+   root->left = new Node(9);
+   root->right = new Node(20);
+   root->right->left = new Node(15);
+   root->right->right = new Node(7);
 
   cout<<maximumDepth(root); 
 }
