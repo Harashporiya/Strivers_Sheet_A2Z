df05d0d9bae8e3c36e0f1b11dac367f9230ecdd7
harash <harash.poriya@gmail.com>
2025-04-17 16:57:48 +0530

    Morris traversal preorder and inorder

---
+++ Strivers_A_to_Z_DSA_COURSE/BinaryTree/MorriesTraversalPreOrder.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data=val;
+        left=NULL;
+        right=NULL;
+    }
+};
+vector<int>  getPreOrder(Node* root){
+    vector<int> res;
+    Node* cur=root;
+    while(cur!=NULL){
+         if(cur->left==NULL){
+            res.push_back(cur->data);
+            cur=cur->right;
+         }else{
+            Node* prev = cur->left;
+            while(prev->right && prev->right!=cur){
+                prev=prev->right;
+            }
+            if(prev->right==NULL){
+                prev->right = cur;
+                res.push_back(cur->data);
+                cur=cur->left;
+            }else{
+                prev->right=NULL;
+               
+                cur=cur->right;
+            }
+         }
+    }
+    return res;
+}
+int main(){
+   Node* root = new Node(10);
+   root->left = new Node(12);
+   root->right = new Node(5);
+   root->left->left = new Node(3);
+   root->left->right = new Node(4);
+   root->left->right->left = new Node(6);
+   root->left->right->right = new Node(7);
+   root->right->left = new Node(5);
+   root->right->left = new Node(11);
+   root->right->right = new Node(2);
+   root->right->right->right = new Node(8);
+
+   
+   vector<int> res = getPreOrder(root); 
+   for(auto it:res){
+    cout<<it<<" ";
+   }
+}
