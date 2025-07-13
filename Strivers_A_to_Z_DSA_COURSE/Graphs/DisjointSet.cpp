45fe58c0bce7cf432c8fc55dc211f0453a7add6a
harash <harash.poriya@gmail.com>
2025-07-13 09:38:26 +0530

    DisJoint set by size

--- Strivers_A_to_Z_DSA_COURSE/Graphs/DisjointSet.cpp
+++ Strivers_A_to_Z_DSA_COURSE/Graphs/DisjointSet.cpp
@@ -1,14 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 class DisJointSet{
-    vector<int> rank,parent;
+    vector<int> rank,parent,size;
     public:
     DisJointSet(int n){
         rank.resize(n+1,0);
         parent.resize(n+1);
-
+        size.resize(n+1);
         for(int i=0;i<=n;i++){
             parent[i]=i;
+            size[i]=1;
         }
     }
 
@@ -34,21 +35,49 @@ class DisJointSet{
             rank[ulp_u]++;
         }
     }
+
+    void unionBySize(int u, int v){
+        int ulp_u = findUpar(u);
+        int ulp_v = findUpar(v);
+        if(ulp_u==ulp_v)return;
+        if(size[ulp_u]<size[ulp_v]){
+            parent[ulp_u]=ulp_v;
+            size[ulp_v]+=size[ulp_u];
+        }else{
+            parent[ulp_v]=ulp_u;
+            size[ulp_u]+=size[ulp_v];
+        }
+    }
 };
 int main(){
-   DisJointSet ds(7);
-   ds.unionByRank(1,2);
-   ds.unionByRank(2,3);
-   ds.unionByRank(4,5);
-   ds.unionByRank(6,7);
-   ds.unionByRank(5,6);
+//    DisJointSet ds(7);
+//    ds.unionByRank(1,2);
+//    ds.unionByRank(2,3);
+//    ds.unionByRank(4,5);
+//    ds.unionByRank(6,7);
+//    ds.unionByRank(5,6);
 
-   if(ds.findUpar(3)==ds.findUpar(7)){
-    cout<<"Same\n";
-   }else cout<<"Not Same\n";
+//    if(ds.findUpar(3)==ds.findUpar(7)){
+//     cout<<"Same\n";
+//    }else cout<<"Not Same\n";
 
-   ds.unionByRank(3,7);
-   if(ds.findUpar(3)==ds.findUpar(7)){
-    cout<<"Same\n";
-   }else cout<<"Not Same\n";
+//    ds.unionByRank(3,7);
+//    if(ds.findUpar(3)==ds.findUpar(7)){
+//     cout<<"Same\n";
+//    }else cout<<"Not Same\n";
+DisJointSet ds(7);
+ds.unionBySize(1,2);
+ds.unionBySize(2,3);
+ds.unionBySize(4,5);
+ds.unionBySize(6,7);
+ds.unionBySize(5,6);
+
+if(ds.findUpar(3)==ds.findUpar(7)){
+ cout<<"Same\n";
+}else cout<<"Not Same\n";
+
+ds.unionBySize(3,7);
+if(ds.findUpar(3)==ds.findUpar(7)){
+ cout<<"Same\n";
+}else cout<<"Not Same\n";
 }
