4062ef10782d7920dc799b215edb703f7e304845
harash <harash.poriya@gmail.com>
2025-07-30 17:54:53 +0530

    Unboundedknapsack

---
+++ Strivers_A_to_Z_DSA_COURSE/AdityaVerma/Dp/unboundedKnapSack.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int knapSack(int W, vector<int>& wt, vector<int>& val, int n) {
+    // DP table banate hai with (n+1) x (W+1) size
+    vector<vector<int>> dp(n + 1, vector<int>(W + 1, 0));
+
+    // Build the table dp[][] bottom-up
+    for (int i = 1; i <= n; i++) {
+        for (int w = 0; w <= W; w++) {
+            if (wt[i - 1] <= w) {
+                // Include ya exclude karo item ko
+                dp[i][w] = max(val[i - 1] + dp[i][w - wt[i - 1]], 
+                               dp[i - 1][w]);
+            } else {
+                // Item nahi le sakte
+                dp[i][w] = dp[i - 1][w];
+            }
+        }
+    }
+
+    return dp[n][W]; 
+}
+
+int main() {
+    int n = 8;
+    vector<int> wt = {1, 2, 3, 4,5,6,7,8};
+    vector<int> val = {1, 5, 8, 9, 10, 17, 17, 20};
+    int W = 8;
+
+    cout << "Maximum value in knapsack = " << knapSack(W, wt, val, n) << endl;
+    return 0;
+}
