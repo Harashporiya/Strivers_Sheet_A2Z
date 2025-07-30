6f6eb57d320357cb0a470ab9af738fa375e63549
harash <harash.poriya@gmail.com>
2025-07-30 14:01:50 +0530

    Longest common subsequences---> Recursive + memorization  apporach

--- Strivers_A_to_Z_DSA_COURSE/AdityaVerma/Dp/lcs.cpp
+++ Strivers_A_to_Z_DSA_COURSE/AdityaVerma/Dp/lcs.cpp
@@ -1,19 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
-int lcs(string s1, string s2, int n, int m){
+int lcs(string s1, string s2, int n, int m, vector<vector<int>>&dp){
     if(n==0 || m==0){
         return 0;
     }
+    if(dp[n][m]!=-1){
+        return dp[n][m];
+    }
 
     if(s1[n-1]==s2[m-1]){
-        return 1+lcs(s1,s2,n-1,m-1);
+        return dp[n][m] =  1+lcs(s1,s2,n-1,m-1,dp);
     }else{
-        return max(lcs(s1,s2,n-1,m),lcs(s1,s2,n,m-1));
+        return dp[n][m] = max(lcs(s1,s2,n-1,m,dp),lcs(s1,s2,n,m-1,dp));
     }
 }
 int main(){
     string s1,s2;
     cin>>s1>>s2;
-
-    cout<<lcs(s1,s2,s1.length(),s2.length());
+     vector<vector<int>> dp(s1.length()+1,vector<int>(s2.length()+1,-1));
+    cout<<lcs(s1,s2,s1.length(),s2.length(),dp);
 }
