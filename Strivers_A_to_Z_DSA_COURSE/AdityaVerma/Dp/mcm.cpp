63df95a2ddf39e5776b6573dc55ff1a285351303
harash <harash.poriya@gmail.com>
2025-08-03 10:37:34 +0530

    Matrix chain multiplication Memorization

--- Strivers_A_to_Z_DSA_COURSE/AdityaVerma/Dp/mcm.cpp
+++ Strivers_A_to_Z_DSA_COURSE/AdityaVerma/Dp/mcm.cpp
@@ -2,16 +2,22 @@
 // Matrix chain multiplication
 #include<bits/stdc++.h>
 using namespace std;
-int mcm(vector<int>&arr,int i,int j){
+int mcm(vector<int>&arr, int i,int j,vector<vector<int>>&dp){
     if(i>=j)return 0;
-
+    
+    if(dp[i][j]!=-1)return dp[i][j];
+    
     int mini=INT_MAX;
-
+    
     for(int k=i;k<=j-1;k++){
-        int ans = mcm(arr,i,k)+mcm(arr,k+1,j)+arr[i-1]*arr[j]*arr[k];
-        mini=min(mini,ans);
+        int tempans = mcm(arr,i,k,dp)+mcm(arr,k+1,j,dp)+arr[i-1]*arr[k]*arr[j];
+        
+        mini=min(mini,tempans);
     }
-    return mini;
+    
+    dp[i][j]=mini;
+    
+    return dp[i][j];
 }
 
 int main(){
@@ -21,6 +27,6 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-
-    cout<<mcm(arr,1,n-1);
+    vector<vector<int>> dp(n, vector<int>(n,-1));
+    cout<<mcm(arr,1,n-1,dp);
 }
