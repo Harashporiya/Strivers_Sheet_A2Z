8adc5f047684591fae0a3325d0fc1cfd8295df3a
harashporiya <harash.poriya@gmail.com>
2026-01-08 18:12:45 +0530

    This code in add memorization

--- Strivers_A_to_Z_DSA_COURSE/dp/maxProductSubsequences.cpp
+++ Strivers_A_to_Z_DSA_COURSE/dp/maxProductSubsequences.cpp
@@ -1,56 +1,66 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// int f(int i, int j, vector<int>& a, vector<int>& b) {
-//     if (i == a.size() || j == b.size())
-//         return INT_MIN;
 
-//     int take = a[i] * b[j];
-//     int next = f(i + 1, j + 1, a, b);
-//     if (next != INT_MIN)
-//         take += max(0, next);
+// void f(int i,vector<int>& nums1,vector<int>& sub,vector<vector<int>> &subsequences){
+//         if(i==nums1.size()){
+//             if(!sub.empty()){
+//                 subsequences.push_back(sub);
+//             }
+//             return;
+//         }
 
-//     int skipA = f(i + 1, j, a, b);
-//     int skipB = f(i, j + 1, a, b);
+//         sub.push_back(nums1[i]);
+//         f(i+1,nums1,sub,subsequences);
+//         sub.pop_back();
+//         f(i+1,nums1,sub,subsequences);
+//     }
+// int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
+//         vector<vector<int>> subsequences1,subsequences2;
+//         vector<int> sub;
+//         f(0,nums1,sub,subsequences1);
+//         int maxi=INT_MIN;
+//         sub.clear();
+//          f(0,nums2,sub,subsequences2);
 
-//     return max({take, skipA, skipB});
+//         for(auto it1:subsequences1){
+//             for(auto it2:subsequences2){
+//                 if(it1.size()!=it2.size())continue;
+//                 int sum=0;
+//                 for(int i=0;i<it1.size();i++){
+//                     sum+=it1[i]*it2[i];
+//                 }
+//                 maxi=max(maxi,sum);
+//          }
+//         }
+
+
+
+//         return maxi;
 // }
-
-void f(int i,vector<int>& nums1,vector<int>& sub,vector<vector<int>> &subsequences){
-        if(i==nums1.size()){
-            if(!sub.empty()){
-                subsequences.push_back(sub);
-            }
-            return;
+int f(int i,int j,vector<int>& nums1, vector<int>& nums2,vector<vector<int>> &dp){
+        if(i==nums1.size()||j==nums2.size()){
+            return INT_MIN;
         }
 
-        sub.push_back(nums1[i]);
-        f(i+1,nums1,sub,subsequences);
-        sub.pop_back();
-        f(i+1,nums1,sub,subsequences);
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+
+        int take=nums1[i]*nums2[j];
+        int n = f(i+1,j+1,nums1,nums2,dp);
+        if(n>0){
+            take+=n;
+        }
+
+        int notTake1=f(i,j+1,nums1,nums2,dp);
+        int notTake2=f(i+1,j,nums1,nums2,dp);
+
+        return dp[i][j]=max({take,notTake1,notTake2});
     }
 int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
-        vector<vector<int>> subsequences1,subsequences2;
-        vector<int> sub;
-        f(0,nums1,sub,subsequences1);
-        int maxi=INT_MIN;
-        sub.clear();
-         f(0,nums2,sub,subsequences2);
-
-        for(auto it1:subsequences1){
-            for(auto it2:subsequences2){
-                if(it1.size()!=it2.size())continue;
-                int sum=0;
-                for(int i=0;i<it1.size();i++){
-                    sum+=it1[i]*it2[i];
-                }
-                maxi=max(maxi,sum);
-         }
-        }
-
-
-
-        return maxi;
+        vector<vector<int>> dp(nums1.size(),vector<int>(nums2.size(),-1));
+       return f(0,0,nums1,nums2,dp);
 }
 
 int main() {
