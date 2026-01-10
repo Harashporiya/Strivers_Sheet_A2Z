3b70eb79f630466c3f0d6352306814bb9fd25ed7
harashporiya <harash.poriya@gmail.com>
2026-01-10 22:35:36 +0530

    this code in memorization but some issue in code time and space complexity error extrace space use recursion 2^n use stack and slove this problem tabulation approches

--- Strivers_A_to_Z_DSA_COURSE/dp/Minimum_ASCII_Delete_Sum_for_Two_Strings.cpp
+++ Strivers_A_to_Z_DSA_COURSE/dp/Minimum_ASCII_Delete_Sum_for_Two_Strings.cpp
@@ -1,15 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
-int f(int i,int j,string s1,string s2){
+int f(int i,int j,string s1,string s2, vector<vector<int>>& dp){
     int n=s1.size();
     int m=s2.size();
+
+    if(dp[i][j]!=-1){
+        return dp[i][j];
+    }
 
     if(i==n){
         int s=0;
         for(int k=j;k<m;k++){
             s+=s2[k];
         }
-        return s;
+        return dp[i][j]=s;
     }
 
     if(j==m){
@@ -17,20 +21,23 @@ int f(int i,int j,string s1,string s2){
         for(int k=i;k<n;k++){
             s+=s1[k];
         }
-        return s;
+        return dp[i][j]=s;
     }
 
     if(s1[i]==s2[j]){
-        return f(i+1,j+1,s1,s2);
+        return f(i+1,j+1,s1,s2,dp);
     }
 
-    int d1=s1[i]+f(i+1,j,s1,s2);
-    int d2=s2[j]+f(i,j+1,s1,s2);
+    int d1=s1[i]+f(i+1,j,s1,s2,dp);
+    int d2=s2[j]+f(i,j+1,s1,s2,dp);
 
-    return min(d1,d2);
+    return dp[i][j]=min(d1,d2);
 }
 int minimumDeleteSum(string s1,string s2){
-    return f(0,0,s1,s2);
+     int n=s1.size();
+    int m=s2.size();
+    vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+    return f(0,0,s1,s2,dp);
 }
 int main(){
     string s1="sea";
