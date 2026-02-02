2b33e163c09fcc6492a0c5528e5bb945f7802cbd
harashporiya <harash.poriya@gmail.com>
2026-02-03 00:22:13 +0530

    This is pick and not pick problem and 0/1 knapsack problem

---
+++ Strivers_A_to_Z_DSA_COURSE/Recursion/subsets.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+void f(int i,vector<vector<int>>&ans,vector<int>&v,vector<int>&nums){
+    if(i==nums.size()){
+        ans.push_back(v);
+        return;
+    }
+
+    v.push_back(nums[i]);
+
+    f(i+1,ans,v,nums);
+
+    v.pop_back();
+
+    f(i+1,ans,v,nums);
+}
+vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> v;
+    //   sort(ans.begin(),ans.end());
+        f(0,ans,v,nums);
+
+        return ans;
+}
+int main(){
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    vector<vector<int>> ans=subsets(arr);
+    
+    for(int i=0;i<ans.size();i++){
+        cout<<"[ ";
+        for(int j=0;j<ans[i].size();j++){
+            cout<<ans[i][j]<<" ";
+        }
+        cout<<"]"<<endl;
+    }
+}
