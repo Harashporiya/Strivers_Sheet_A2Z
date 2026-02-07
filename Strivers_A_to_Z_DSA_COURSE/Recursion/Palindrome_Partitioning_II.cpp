e91b10d37421011cae84bd1303a350e10b1d5ba5
harashporiya <harash.poriya@gmail.com>
2026-02-08 00:31:55 +0530

    This is optimal approaches palindrome partitioning_II

--- Strivers_A_to_Z_DSA_COURSE/Recursion/Palindrome_Partitioning_II.cpp
+++ Strivers_A_to_Z_DSA_COURSE/Recursion/Palindrome_Partitioning_II.cpp
@@ -1,30 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-bool isPalindrome(string s,int st,int ed){
-        while(st<=ed){
-            if(s[st++]!=s[ed--])return false;
-        }
-        return true;
-    }
-   int f(int ind,string s,vector<int>&dp){
-    if(ind==s.size()){
-       return 0;
-    }
-    if(dp[ind]!=-1)return dp[ind];
-    int mini=INT_MAX;
-     for(int i=ind;i<s.size();i++){
-        if(isPalindrome(s,ind,i)){
-           int cut =1+ f(i+1,s,dp);
-            mini=min(mini,cut);
-        }
-     }
-     return dp[ind]=mini;
-}
 int minCut(string s) {
         int n=s.size();
-        vector<int> dp(n+1,-1);
-        return f(0,s,dp)-1;
+        vector<vector<bool>> checkPal(n,vector<bool>(n,false));
+
+        for(int i=n-1;i>=0;i--){
+            for(int j=i;j<n;j++){
+                if(s[i]==s[j]&&(j-i<=2 || checkPal[i+1][j-1])){
+                    checkPal[i][j]=true;
+                }
+            }
+        }
+        
+        vector<int> dp(n+1,0);
+        dp[n]=0;
+
+        for(int i=n-1;i>=0;i--){
+            int mini=INT_MAX;
+          for(int j=i;j<s.size();j++){
+            if(checkPal[i][j]){
+            int cut =1+ dp[j+1];
+                mini=min(mini,cut);
+            }
+     }
+      dp[i]=mini;
+        }
+        return dp[0]-1;
 }
 
 int main(){
