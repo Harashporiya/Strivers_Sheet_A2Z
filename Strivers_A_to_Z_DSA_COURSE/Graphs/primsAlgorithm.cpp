15f6c8eaaa584c9ab1f53271b888cc1929a9c350
harashporiya <harash.poriya@gmail.com>
2026-02-14 09:59:44 +0530

    Prim's algorithm in use priority queue

--- Strivers_A_to_Z_DSA_COURSE/Graphs/primsAlgorithm.cpp
+++ Strivers_A_to_Z_DSA_COURSE/Graphs/primsAlgorithm.cpp
@@ -1,41 +1,75 @@
 #include<bits/stdc++.h>
 using namespace std;
  vector<pair<pair<int,int>,int>> primsAlgorithm(int V,vector<vector<int>> &edges){
-    vector<vector<pair<int,int>>> adj(V);
+    // vector<vector<pair<int,int>>> adj(V);
 
-    for(int i=0;i<edges.size();i++){
-        adj[edges[i][0]].push_back({edges[i][1],edges[i][2]});
-        adj[edges[i][1]].push_back({edges[i][0],edges[i][2]});
-    }
+    // for(int i=0;i<edges.size();i++){
+    //     adj[edges[i][0]].push_back({edges[i][1],edges[i][2]});
+    //     adj[edges[i][1]].push_back({edges[i][0],edges[i][2]});
+    // }
 
-    vector<int> key(V,1e9);
-    vector<int> parent(V,-1);   
-    vector<bool> mst(V,false);
+    // vector<int> key(V,1e9);
+    // vector<int> parent(V,-1);   
+    // vector<bool> mst(V,false);
 
-    key[0]=0;
+    // key[0]=0;
 
-    for(int i=0;i<V;i++){
-        int mini=INT_MAX;
-        int u;
-        for(int j=0;j<V;j++){
-            if(mst[j]==false && key[j]<mini){
-                mini=key[j];
-                u=j;
+    // for(int i=0;i<V;i++){
+    //     int mini=INT_MAX;
+    //     int u;
+    //     for(int j=0;j<V;j++){
+    //         if(mst[j]==false && key[j]<mini){
+    //             mini=key[j];
+    //             u=j;
+    //         }
+    //     }
+    //     mst[u]=true;
+    //     for(auto it:adj[u]){
+    //         if(mst[it.first]==false && it.second<key[it.first]){
+    //             key[it.first]=it.second;
+    //             parent[it.first]=u;
+    //         }
+    //     }
+    // }
+    // vector<pair<pair<int,int>,int>> res;
+    // for(int i=1;i<V;i++){
+    //     res.push_back({{parent[i],i},key[i]});
+    // }
+    // return res;
+
+            // code here
+        vector<vector<pair<int,int>>> adj(V);
+        int sum=0;
+        for(int i=0;i<edges.size();i++){
+            adj[edges[i][0]].push_back({edges[i][1],edges[i][2]});
+            adj[edges[i][1]].push_back({edges[i][0],edges[i][2]});
+        }
+        
+        //vector<int> key(V,1e9);
+        vector<int> mst(V,false);
+        //vector<int> parent(V,-1);
+        
+       // key[0]=0;
+        priority_queue<pair<int,int>, vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        pq.push({0,0});
+
+        
+        while(!pq.empty()){
+            int node=pq.top().second;
+            int w=pq.top().first;
+            pq.pop();
+            
+            if(mst[node])continue;
+            mst[node]=true;
+            sum+=w;
+            
+            for(auto it:adj[node]){
+                if(!mst[it.first]){
+                    pq.push({it.second,it.first});
+                }
             }
         }
-        mst[u]=true;
-        for(auto it:adj[u]){
-            if(mst[it.first]==false && it.second<key[it.first]){
-                key[it.first]=it.second;
-                parent[it.first]=u;
-            }
-        }
-    }
-    vector<pair<pair<int,int>,int>> res;
-    for(int i=1;i<V;i++){
-        res.push_back({{parent[i],i},key[i]});
-    }
-    return res;
+        return sum;
 }
 int main(){
     int V = 5;
