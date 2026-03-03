5d5a14c3aca1757878c12dad69ab191b75070ad4
harashporiya <harash.poriya@gmail.com>
2026-03-03 08:41:56 +0530

    Subset sum equal to target memorization+tabulation+space optimaizi approachess

--- Strivers_A_to_Z_DSA_COURSE/dp/subSetSumEqualTarget.cpp
+++ Strivers_A_to_Z_DSA_COURSE/dp/subSetSumEqualTarget.cpp
@@ -1,45 +1,26 @@
-#include <bits/stdc++.h>
-using namespace std;
+bool isSubsetSum(vector<int>& arr, int sum) {
+    int n = arr.size();
+    vector<bool> prev(sum + 1, false);
 
-class Solution {
-public:
-    bool isSubsetSum(vector<int>& arr, int sum) {
-        int n = arr.size();
-        vector<vector<bool>> dp(n, vector<bool>(sum + 1, false));
+    prev[0] = true;
+    if (arr[0] <= sum)
+        prev[arr[0]] = true;
 
-        // Base Case
-        for (int i = 0; i < n; i++)
-            dp[i][0] = true;
+    for (int i = 1; i < n; i++) {
+        vector<bool> curr(sum + 1, false);
+        curr[0] = true;
 
-        if (arr[0] <= sum)
-            dp[0][arr[0]] = true;
+        for (int target = 1; target <= sum; target++) {
+            bool notTake = prev[target];
+            bool take = false;
 
-        for (int i = 1; i < n; i++) {
-            for (int target = 1; target <= sum; target++) {
+            if (target >= arr[i])
+                take = prev[target - arr[i]];
 
-                bool notTake = dp[i - 1][target];
-                bool take = false;
-
-                if (target >= arr[i])
-                    take = dp[i - 1][target - arr[i]];
-
-                dp[i][target] = take || notTake;
-            }
+            curr[target] = take || notTake;
         }
-
-        return dp[n - 1][sum];
+        prev = curr;
     }
-};
 
-int main() {
-    Solution obj;
-    vector<int> arr = {3, 34, 4, 12, 5, 2};
-    int sum = 9;
-
-    if (obj.isSubsetSum(arr, sum))
-        cout << "Subset exists";
-    else
-        cout << "Subset does not exist";
-
-    return 0;
+    return prev[sum];
 }
