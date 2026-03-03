4f3274ad4b6948f97c8e481f5c1d1572eb65e9dd
harashporiya <harash.poriya@gmail.com>
2026-03-03 08:41:04 +0530

    Subset sum equal to target memorization+tabulation approachess

--- Strivers_A_to_Z_DSA_COURSE/dp/subSetSumEqualTarget.cpp
+++ Strivers_A_to_Z_DSA_COURSE/dp/subSetSumEqualTarget.cpp
@@ -3,26 +3,31 @@ using namespace std;
 
 class Solution {
 public:
-    bool solve(int ind, vector<int>& arr, int target, vector<vector<int>>& dp) {
-        if (target == 0) return true;
-        if (ind == 0) return (arr[0] == target);
-
-        if (dp[ind][target] != -1)
-            return dp[ind][target];
-
-        bool notTake = solve(ind - 1, arr, target, dp);
-        bool take = false;
-
-        if (target >= arr[ind])
-            take = solve(ind - 1, arr, target - arr[ind], dp);
-
-        return dp[ind][target] = take || notTake;
-    }
-
     bool isSubsetSum(vector<int>& arr, int sum) {
         int n = arr.size();
-        vector<vector<int>> dp(n, vector<int>(sum + 1, -1));
-        return solve(n - 1, arr, sum, dp);
+        vector<vector<bool>> dp(n, vector<bool>(sum + 1, false));
+
+        // Base Case
+        for (int i = 0; i < n; i++)
+            dp[i][0] = true;
+
+        if (arr[0] <= sum)
+            dp[0][arr[0]] = true;
+
+        for (int i = 1; i < n; i++) {
+            for (int target = 1; target <= sum; target++) {
+
+                bool notTake = dp[i - 1][target];
+                bool take = false;
+
+                if (target >= arr[i])
+                    take = dp[i - 1][target - arr[i]];
+
+                dp[i][target] = take || notTake;
+            }
+        }
+
+        return dp[n - 1][sum];
     }
 };
 
