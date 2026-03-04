1009aa22542883338a7c454183caec7739057d1b
harashporiya <harash.poriya@gmail.com>
2026-03-04 08:37:56 +0530

    Optimal approach

--- Strivers_A_to_Z_DSA_COURSE/DailQuestions/Special_Positions_in_a_Binary_Matrix.cpp
+++ Strivers_A_to_Z_DSA_COURSE/DailQuestions/Special_Positions_in_a_Binary_Matrix.cpp
@@ -6,33 +6,24 @@ public:
     int numSpecial(vector<vector<int>>& mat) {
         int n = mat.size();
         int m = mat[0].size();
+
+        vector<int> row(n, 0), col(m, 0);
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(mat[i][j] == 1) {
+                    row[i]++;
+                    col[j]++;
+                }
+            }
+        }
+
         int ans = 0;
 
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
-
-                if(mat[i][j] == 1) {
-
-                    bool check = true;
-
-                    // Check row
-                    for(int col = 0; col < m; col++) {
-                        if(col != j && mat[i][col] == 1) {
-                            check = false;
-                            break;
-                        }
-                    }
-
-                    // Check column
-                    for(int row = 0; row < n; row++) {
-                        if(row != i && mat[row][j] == 1) {
-                            check = false;
-                            break;
-                        }
-                    }
-
-                    if(check) ans++;
-                }
+                if(mat[i][j] == 1 && row[i] == 1 && col[j] == 1)
+                    ans++;
             }
         }
 
