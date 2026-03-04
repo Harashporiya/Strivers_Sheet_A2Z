b2590b1b67dde85a656f5cf5d923be6d61be7d4f
harashporiya <harash.poriya@gmail.com>
2026-03-04 08:38:48 +0530

    All approach in one file

--- Strivers_A_to_Z_DSA_COURSE/DailQuestions/Special_Positions_in_a_Binary_Matrix.cpp
+++ Strivers_A_to_Z_DSA_COURSE/DailQuestions/Special_Positions_in_a_Binary_Matrix.cpp
@@ -3,7 +3,45 @@ using namespace std;
 
 class Solution {
 public:
-    int numSpecial(vector<vector<int>>& mat) {
+    // Brute Force Approach (O(1) space)
+    int numSpecialBruteForce(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+        int ans = 0;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+
+                if(mat[i][j] == 1) {
+
+                    bool check = true;
+
+                    // Check row
+                    for(int col = 0; col < m; col++) {
+                        if(col != j && mat[i][col] == 1) {
+                            check = false;
+                            break;
+                        }
+                    }
+
+                    // Check column
+                    for(int row = 0; row < n; row++) {
+                        if(row != i && mat[row][j] == 1) {
+                            check = false;
+                            break;
+                        }
+                    }
+
+                    if(check) ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    // Optimized Approach (O(n + m) space)
+    int numSpecialOptimized(vector<vector<int>>& mat) {
         int n = mat.size();
         int m = mat[0].size();
 
@@ -29,18 +67,28 @@ public:
 
         return ans;
     }
+
+    // Function to choose approach
+    int numSpecial(vector<vector<int>>& mat, bool useOptimized) {
+        if(useOptimized) {
+            return numSpecialOptimized(mat);
+        } else {
+            return numSpecialBruteForce(mat);
+        }
+    }
 };
 
 int main() {
 
     vector<vector<int>> mat = {
-        {1,0,0},
-        {0,0,1},
-        {1,0,0}
+        {1, 0, 0},
+        {0, 0, 1},
+        {1, 0, 0}
     };
 
+    bool useOptimized = true; // Change this to false to use Brute Force approach
     Solution obj;
-    cout << "Special Positions: " << obj.numSpecial(mat);
+    cout << "Special Positions: " << obj.numSpecial(mat, useOptimized);
 
     return 0;
 }
